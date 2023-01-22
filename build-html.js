dce166706f18fcc9f6408f937b5575fa3a36b7d3
ritafomkina <ritakisil@MacBook-Air-Margarita.local>
2023-01-22 12:53:57 +0300

    feat: keys

--- build-html.js
+++ build-html.js
@@ -1,3 +1,229 @@
+// Map с данными о каждой клавише:
+
+const keysInfo = new Map([
+  ['Backquote', {
+    enValue: '§', ruValue: '>', addEnValue: '±', addRuValue: '<', classNames: ['key', 'char'],
+  }],
+  ['Digit1', {
+    enValue: '1', ruValue: '1', addEnValue: '!', addRuValue: '!', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit2', {
+    enValue: '2', ruValue: '2', addEnValue: '@', addRuValue: '"', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit3', {
+    enValue: '3', ruValue: '3', addEnValue: '#', addRuValue: '№', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit4', {
+    enValue: '4', ruValue: '4', addEnValue: '$', addRuValue: '%', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit5', {
+    enValue: '5', ruValue: '5', addEnValue: '%', addRuValue: ':', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit6', {
+    enValue: '6', ruValue: '6', addEnValue: '^', addRuValue: ',', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit7', {
+    enValue: '7', ruValue: '7', addEnValue: '&', addRuValue: '.', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit8', {
+    enValue: '8', ruValue: '8', addEnValue: '*', addRuValue: ';', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit9', {
+    enValue: '9', ruValue: '9', addEnValue: '(', addRuValue: '(', classNames: ['key', 'char', 'num'],
+  }],
+  ['Digit0', {
+    enValue: '0', ruValue: '0', addEnValue: ')', addRuValue: ')', classNames: ['key', 'char', 'num'],
+  }],
+  ['Minus', {
+    enValue: '-', ruValue: '-', addEnValue: '_', addRuValue: '_', classNames: ['key', 'char', 'num'],
+  }],
+  ['Equal', {
+    enValue: '=', ruValue: '=', addEnValue: '+', addRuValue: '+', classNames: ['key', 'char', 'num'],
+  }],
+  ['Backspace', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['Tab', {
+    enValue: '  ', ruValue: '  ', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['KeyQ', {
+    enValue: 'Q', ruValue: 'й', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyW', {
+    enValue: 'W', ruValue: 'ц', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyE', {
+    enValue: 'E', ruValue: 'у', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyR', {
+    enValue: 'R', ruValue: 'к', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyT', {
+    enValue: 'T', ruValue: 'е', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyY', {
+    enValue: 'Y', ruValue: 'н', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyU', {
+    enValue: 'U', ruValue: 'г', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyI', {
+    enValue: 'I', ruValue: 'ш', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyO', {
+    enValue: 'O', ruValue: 'щ', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyP', {
+    enValue: 'P', ruValue: 'з', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['BracketLeft', {
+    enValue: '[', ruValue: 'х', addEnValue: '{', addRuValue: '', classNames: ['key', 'char', 'ru-lttr'],
+  }],
+  ['BracketRight', {
+    enValue: ']', ruValue: 'ъ', addEnValue: '}', addRuValue: '', classNames: ['key', 'char', 'ru-lttr'],
+  }],
+  ['Backslash', {
+    enValue: '\u002F', ruValue: 'ё', addEnValue: '|', addRuValue: '', classNames: ['key', 'char', 'ru-lttr'],
+  }],
+  ['CapsLock', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['KeyA', {
+    enValue: 'A', ruValue: 'ф', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyS', {
+    enValue: 'S', ruValue: 'ы', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyD', {
+    enValue: 'D', ruValue: 'в', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyF', {
+    enValue: 'F', ruValue: 'а', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyG', {
+    enValue: 'G', ruValue: 'п', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyH', {
+    enValue: 'H', ruValue: 'р', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyJ', {
+    enValue: 'J', ruValue: 'о', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyK', {
+    enValue: 'K', ruValue: 'л', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyL', {
+    enValue: 'L', ruValue: 'д', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['Semicolon', {
+    enValue: ';', ruValue: 'ж', addEnValue: ':', addRuValue: '', classNames: ['key', 'char', 'ru-lttr'],
+  }],
+  ['Quote', {
+    enValue: '\'', ruValue: 'э', addEnValue: '"', addRuValue: '', classNames: ['key', 'char', 'ru-lttr'],
+  }],
+  ['Enter', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['ShiftLeft', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['IntlBackslash', {
+    enValue: '`', ruValue: ']', addEnValue: '~', addRuValue: '[', classNames: ['key', 'char'],
+  }],
+  ['KeyZ', {
+    enValue: 'Z', ruValue: 'я', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyX', {
+    enValue: 'X', ruValue: 'ч', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyC', {
+    enValue: 'C', ruValue: 'с', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyV', {
+    enValue: 'V', ruValue: 'м', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyB', {
+    enValue: 'B', ruValue: 'и', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyN', {
+    enValue: 'N', ruValue: 'т', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['KeyM', {
+    enValue: 'M', ruValue: 'ь', addEnValue: '', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['Comma', {
+    enValue: ',', ruValue: 'б', addEnValue: '<', addRuValue: '', classNames: ['key', 'char', 'en-lttr', 'ru-lttr'],
+  }],
+  ['Period', {
+    enValue: '.', ruValue: 'ю', addEnValue: '>', addRuValue: '', classNames: ['key', 'char', 'ru-lttr'],
+  }],
+  ['Slash', {
+    enValue: '/', ruValue: '/', addEnValue: '?', addRuValue: '?', classNames: ['key', 'char'],
+  }],
+  ['ShiftRight', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['Fn', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['ControlLeft', {
+    enValue: 'control', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['AltLeft', {
+    enValue: 'option', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['MetaLeft', {
+    enValue: 'command', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['Space', {
+    enValue: ' ', ruValue: ' ', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['MetaRight', {
+    enValue: 'command', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['AltRight', {
+    enValue: 'option', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['ArrowLeft', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['ArrowContainer', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['container'],
+  }],
+  ['ArrowRight', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+]);
+
+const arrowContainer = new Map([
+  ['ArrowUp', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+  ['ArrowDown', {
+    enValue: '', ruValue: '', addEnValue: '', addRuValue: '', classNames: ['key', 'mod'],
+  }],
+]);
+
+// пару дополнительных функций:
+
+function fillWithContent(elsInfo, els, propName) {
+  els.forEach((el) => {
+    const targetInfo = elsInfo.get(el.id);
+    el.innerHTML = targetInfo[propName];
+  });
+}
+
+function createAddEls(parentEl, childTagName, elsInfo, propName) {
+  parentEl.forEach((char) => {
+    const targetInfo = elsInfo.get(char.id);
+    if (targetInfo[propName]) {
+      const child = document.createElement(childTagName);
+      child.innerHTML = targetInfo[propName];
+      char.appendChild(child);
+    }
+  });
+}
+
 // класс, который строит страницу
 
 class HTMLBuilder {
@@ -6,8 +232,8 @@ class HTMLBuilder {
     this.wrapper = document.createElement(tagName);
     this.wrapper.classList.add(...classNames);
     document.body.appendChild(this.wrapper);
-     // создадим на всякий случай контейнер как свойство объекта первого уровня:
-     this.container = [];
+    // создадим на всякий случай контейнер как свойство объекта первого уровня:
+    this.container = [];
   }
 
   // создаем роидтеля для детей и потомков:
@@ -24,7 +250,7 @@ class HTMLBuilder {
     className = null,
     attributes = null,
     num = 1,
-    save = false,
+    // save = false,
   ) {
     let count = num;
     let child;
@@ -42,8 +268,13 @@ class HTMLBuilder {
       source.appendChild(child);
       /* если создаем несколько идентичных элементов, положим ссылку на них в свойство-контейнер,
       чтобы легко получить доступ к ним (как иначе получить доступ к ним из класса - хз): */
-      if (save) this.container.push(child);
+      this.container.push(child);
     } while (count > 0);
+    return this;
+  }
+
+  cleanContainer() {
+    this.container = [];
     return this;
   }
 
@@ -52,18 +283,24 @@ class HTMLBuilder {
     const parent = document.querySelector(parentSelector);
     // console.log(this.source);
     parent.append(this.source);
+    return this;
   }
 
-  setID(IDlist) {
-    this.container.forEach((el, i) => el.setAttribute('id', IDlist[i]));
-  }
-
-  setClass(objInfo) {
-    this.container.forEach((el) => {
-      const elInObj = objInfo.get(el.id);
+  setAttrs(IDlist, elsInfo) {
+    this.container.forEach((el, i) => {
+      el.setAttribute('id', IDlist[i]);
+      const elInObj = elsInfo.get(el.id);
       el.classList.add(...elInObj.classNames);
     });
+    return this;
   }
+
+  // setClass(objInfo) {
+  //   this.container.forEach((el) => {
+  //     const elInObj = objInfo.get(el.id);
+  //     el.classList.add(...elInObj.classNames);
+  //   });
+  // }
 
   addIdenticalChilds(
     elsInfo,
@@ -85,13 +322,12 @@ class HTMLBuilder {
     });
   }
 
-  fillElements(className, elsInfo, propName) {
+  fillWithContent(className, elsInfo, propName) {
     // получаем массив необходимых нам элементов иэ контейнера по их классу:
     const els = this.container.filter((el) => el.classList.contains(className));
-    for (let i = 0; i < els.length; i += 1) {
-      const targetInfo = elsInfo.get(els[i].id);
-      els[i].innerHTML = targetInfo[propName];
-    }
+    // вызывае внешнюю функцию с массивом элементов:
+    fillWithContent(elsInfo, els, propName);
+    return this;
   }
 }
 
@@ -112,40 +348,38 @@ keyboard.setParent('fragment')
 
 keyboard.setParent('fragment')
   .createElement('div', 'keys-area')
-  .createElement('button', null, null, keysInfo.size, true)
+  .cleanContainer()
+  .createElement('button', null, null, keysInfo.size)
   .becomeChildOf('.keyboard');
 
 const IDlist = Array.from(keysInfo.keys());
 
-keyboard.setID(IDlist);
-keyboard.setClass(keysInfo);
-keyboard.fillElements('mod', keysInfo, 'enValue');
-
+keyboard.setAttrs(IDlist, keysInfo)
+  .fillWithContent('mod', keysInfo, 'enValue')
+  .cleanContainer();
 
 keyboard.setParent('fragment')
   .createElement('button', 'key', null, arrowContainer.size)
   .becomeChildOf('#ArrowContainer');
 
-
-
 const arrowIDlist = Array.from(arrowContainer.keys());
-const cont = document.querySelector('#ArrowContainer');
-// keyboard.setID.call(cont, arrowIDlist);
 
-// keyboard.fillElements('container', arrowContainer, 'enValue');
+keyboard.setAttrs(arrowIDlist, arrowContainer)
+  .fillWithContent('mod', arrowContainer, 'enValue')
+  .cleanContainer();
 
 // console.log(keyboard.wrapper); // <---- вот этот консоль лог
 // попеременно выводит keyboard.wrapper в виде объекта и тега
 
-function fillIn(lang) {
+function switchTo(lang, elsInfo) {
+  const chars = Array.from(document.querySelectorAll('.char'));
   if (lang === 'EN') {
-    keyboard.fillElements('char', keysInfo, 'enValue');
-    keyboard.addIdenticalChilds(keysInfo, 'addEnValue', 'span');
+    fillWithContent(elsInfo, chars, 'enValue');
+    createAddEls(chars, 'span', elsInfo, 'addEnValue');
   }
   if (lang === 'RU') {
-    keyboard.fillElements('char', keysInfo, 'ruValue');
-    keyboard.addIdenticalChilds(keysInfo, 'addRuValue', 'span');
+    fillWithContent(elsInfo, chars, 'ruValue');
+    createAddEls(chars, 'span', elsInfo, 'addRuValue');
   }
 }
-fillIn('EN');
-// console.log(keysInfo.size);
+switchTo('EN', keysInfo);
